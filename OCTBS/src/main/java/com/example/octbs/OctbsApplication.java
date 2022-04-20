6c7d8c23b6064c85cf6ee51ecfa5a3605ff8d90b
vickkybopche <vickkybopche2017@gmail.com>
2022-04-20 19:48:50 +0530

    Added project

---
+++ OCTBS/src/main/java/com/example/octbs/OctbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.octbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctbsApplication.class, args);
+	}
+
+}
